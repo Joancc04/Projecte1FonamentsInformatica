d6b389a06ac1273c2162076c3e45e400d1033ac8
Joancc04 <joancolillas2004@gmail.com>
2022-10-07 12:04:09 +0200

    modificació filtre

--- filtre.sh
+++ filtre.sh
@@ -5,13 +5,13 @@ IFS=","
 
 while read id type title director 
 do
-	echo -e "\e[1;33m$user \
+	echo -e "\e[1;35m$user \
 	================================\e[0m\n\
 	
-	Id : \t $id\n\
-	Tipus : \t $type\n\
-	Títol : \t $title\n"
+	Id    :\t $id\n\
+	Tipus :\t $type\n\
+	Títol :\t $title\n"
 	
-done<netflix_titles.csv
+done<titles.cvs
 IFS=$OLDIFS
 
