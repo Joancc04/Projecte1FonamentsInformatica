9a5b15b987cb1a7162481b041d86e6a87890fb00
Joancc04 <joancolillas2004@gmail.com>
2022-10-13 11:53:23 +0200

    Apartats 1, 2 i 3 completament acabats, i varies modificacions i automatitzacions

---
+++ elim.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm ShowData.csv
+rm MovieData.csv
+rm Debris.csv
--- ext.sh
+++ ext.sh
@@ -1,12 +1,38 @@
 #! /bin/bash
 
-#fem servir la comanda "sed" per tal d'elimnar totes les rows que continguin
-#el text MOVIE i SHOW del fitxer csv.
+./elim.sh
+echo Creant arxius de dades...
+touch ShowData.csv
+touch MovieData.csv
+touch Debris.csv
 
-# sed -i '/MOVIE/d' ShowData.csv #borrem pelis i ens quedem amb les series
-# sed -i '/SHOW/d' MovieData.csv #borrem series i ens quedem amb pelis
 
-# awk -F "," ' NR > 1 { if ($1 ~ "ts[0-9]*") print $0}' ShowData.csv > ShowData2.csv
-# awk -F "," ' NR > 1 { if ($1 ~ "ts[0-9]*") print $0}' MovieData.csv > MovieData2.csv
+# sed -i '/MOVIE/d' ShowData.csv 
+# sed -i '/SHOW/d' MovieData.csv 
+# echo Netejant dades...
 
-awk -F "," ' NR > 1 { if (($2 !~ /^[A-Z]/) && ($2 !~ /^[a-z]/) && ($2 !~ /^[0-9]/) && ($2 !~ /^ /)) print $0}' ShowData2.csv  > asdf.csv 
+
+
+echo Classificant i netejant dades...
+awk -F "," ' NR > 1 { if (($1 ~ "ts[0-9]") && (($2 ~ /^[A-Z]/) || ($2 ~ /^[0-9]/))) print $0}' RawData.csv > ShowData.csv
+awk -F "," ' NR > 1 { if (($1 ~ "tm[0-9]") && (($2 ~ /^[A-Z]/) || ($2 ~ /^[0-9]/))) print $0}' RawData.csv > MovieData.csv
+
+awk -F "," ' NR > 1 { if ((($1 !~ "tm[0-9]") && ($1 !~ "ts[0-9]")) || (($2 !~ /^[A-Z]/) && ($2 !~ /^[0-9]/))) print $0}' RawData.csv  > Debris.csv 
+
+
+# awk -F "," ' NR > 1 { if (($1 !~ "tm[0-9]*") || ($2 !~ /^[A-Z]/) || ($2 !~ /^[a-z]/) || ($2 !~ /^[0-9]/) || ($2 !~ /^ /)) print $0}' MovieData.csv  > DebrisM.csv 
+
+
+echo En total shan eliminat
+awk 'END{print NR}' Debris.csv
+echo linies
+
+echo I queden netes:
+awk 'END{print NR}' MovieData.csv
+echo pel·lícules i
+awk 'END{print NR}' ShowData.csv
+echo series, dun total de:
+awk 'END{print NR}' RawData.csv
+echo linies
+
+echo Finalitzat!
