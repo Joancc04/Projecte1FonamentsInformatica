2c733e05773b06f4585cf46df62b351119cd4302
Joancc04 <joancolillas2004@gmail.com>
2022-10-17 13:32:38 +0200

    +bugs arreglats i intent apartat 5

--- elim.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm Shows.csv
-rm Movies.csv
-rm Debris.csv
-rm Debrist.csv
-rm Debrisc.csv
--- ext.sh
+++ ext.sh
@@ -1,6 +1,24 @@
 #! /bin/bash
 
-./elim.sh
+#funció eliminar dades
+elim () {
+    rm Shows.csv
+    rm Movies.csv
+    rm Debris.csv
+    rm Debrisid.csv
+    rm Debrisc.csv
+}
+
+echo "Tens dades anteriors? (S/n)"
+read a 
+
+if [ $a == "S" ];
+then
+    elim
+    echo "S'han borrat les dades anteriors, procedim a substituir-les."
+fi
+
+
 echo Creant arxius de dades...
 touch Shows.csv  #arxiu amb dades netes de shows
 touch Movies.csv #arxiu amb dades netes de pelis
@@ -9,15 +27,19 @@ touch Debrisid.csv #debris ids (contador)
 touch Debrisc.csv #debris columnes 11 - 15 (contador)
 
 
+
 # sed -i '/MOVIE/d' ShowData.csv 
 # sed -i '/SHOW/d' MovieData.csv 
 # echo Netejant dades...
+
+
 
 #trasllat i dividit de les dades útils
 echo Classificant i netejant dades...
 awk -F "," ' NR > 1 { if ( (($1 ~ "ts[0-9]") && ( ($11 != "") && ($12 != "") && ($13 != "") && ($14 != "") && ($15 ~ /^[0-9]/))    ) && (($2 ~ /^[A-Z]/) || ($2 ~ /^[0-9]/))) print $0}' RawData.csv > Shows.csv
 awk -F "," ' NR > 1 { if ( (($1 ~ "tm[0-9]") && ( ($11 != "") && ($12 != "") && ($13 != "") && ($14 != "") && ($15 ~ /^[0-9]/))    ) && (($2 ~ /^[A-Z]/) || ($2 ~ /^[0-9]/))) print $0}' RawData.csv > Movies.csv
-
+awk -F "," '{printf "%s%.2f\n", $16, $13/$14}' Movies.csv
+# -F "," = IFS
 
 #eliminat de les dades inservibles
 awk -F "," ' NR > 1 { if (     (($1 !~ "ts[0-9]") && ($1 !~ "tm[0-9]"))    ||     (($2 !~ /^[A-Z]/) && ($2 !~ /^[0-9]/))    ||    (($11 == "") || ($12 == "") || ($13 == "") || ($14 == "") || ($15 !~ /^[0-9]/))      ) print $0}' RawData.csv  > Debris.csv 
@@ -25,7 +47,7 @@ awk -F "," ' NR > 1 { if (($1 !~ "tm[0-9]") && ($1 !~ "ts[0-9]") && ($1 !~ ","))
 awk -F "," ' NR > 1 { if ( (($1 ~ "ts[0-9]") || ($1 ~ "tm[0-9]")) && ( ($11 == "") || ($12 == "") || ($13 == "") || ($14 == "") || ($15 !~ /^[0-9]/)  )) print $0}' RawData.csv > Debrisc.csv
 sed -i '/",,,,,,,,,,,,,,"/d' Debrisid.csv
 
-
+#contados
 echo Finalitzat!
 echo
 awk 'END{ print NR, "línies eliminades degut a id errònies"}' Debrisid.csv
@@ -36,6 +58,20 @@ awk 'END{print NR, "línies de pel·lícules netes"}' Movies.csv
 awk 'END{print NR, "línies de series netes"}' Shows.csv
 echo
 awk 'END{print NR, "línies totals en el fitxer de dades original"}' RawData.csv
+echo
+#NR = Number of Records
+
+#afegim els títols fora dels contadors per tal que no influeixin
+sed -i '1i"id,title,type,description,release_year,age_certification,runtime,genres,production_countries, ,imdb_id,imdb_score,imdb_votes,tmdb_popularity,tmdb_score,imdb_reliability,tmdb_reliability"' Movies.csv
+sed -i '1i"id,title,type,description,release_year,age_certification,runtime,genres,production_countries,seasons,imdb_id,imdb_score,imdb_votes,tmdb_popularity,tmdb_score,imdb_reliability,tmdb_reliability"' Shows.csv
 
 
-#awk  -F "," '{printf "%s,%s,%4.2f\n", $17 , $17, $13/$14 }' Movies.csv
+echo "T'agradaria conservar les dades? (S/n)"
+read b 
+if [ $b != "S" ];
+then
+    elim
+    echo "S'han borrat les dades"
+fi
+
+
