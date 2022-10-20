b9f692c87f6860034c8004ee2ed7859b619b6a19
Joancc04 <joancolillas2004@gmail.com>
2022-10-20 16:48:13 +0200

    apartat 5 casi funcionant

--- ext.sh
+++ ext.sh
@@ -8,6 +8,7 @@ elim () {
     rm Debrisid.csv
     rm Debrisc.csv
     rm MS.csv
+    rm prova.csv
 }
 
 echo "Tens dades anteriors? (S/n)"
@@ -27,6 +28,7 @@ touch Debris.csv #debris total (contador)
 touch Debrisid.csv #debris ids (contador)
 touch Debrisc.csv #debris columnes 11 - 15 (contador)
 touch MS.csv
+touch prova.csv
 
 
 
@@ -67,23 +69,23 @@ echo
 #NR = Number of Records
 
 
-awk -v maxp=0 '{if($13>maxp){want=$16; maxp=$13}}END{print want} ' MS.csv
+awk -F "," 'BEGIN{maxv=0} { if($13 > maxv) maxv=$13} END{print "El número més gran imdb_votes és:", maxv}' MS.csv #troba el màxim de la columna 13
+awk -F "," 'BEGIN{maxp=0} { if($14 > maxp) maxp=$14} END{print "El número més gran tmdb_popularity és:", maxp}' MS.csv #troba el màxim de la columna 14
 
-awk -F "," 'BEGIN {maxp=0} { if ($13 > maxp) maxp=$13} END {print maxp}' MS.csv #és la que està bé
 
-# awk -F "," '{$16=($13/$14)}' MS.csv
-# 'BEGIN DFS="," {$16=()}
+awk -F "," '{print $0, $12*($13/(2268288))}' MS.csv > prova.csv
+# paste MS.csv prova.csv | awk '{print $6, $NF}' > test3.txt
+echo "Títols als documents? (S/n)"
+read c
 
-awk -F "," 'BEGIN{a=795222}{if ($13>a) a=$13 fi} END{print a}' MS.csv
-awk -F "," 'BEGIN{a=0}{if ($13>a) a=$13 fi} END{print a}' MS.csv
 
-# awk -F "," 'BEGIN{print($13/$14), $16}' MS.csv
-# awk -F "," '$16={printf "%s%f\n", $13/$14}' Movies.csv
-
-# afegim els títols fora dels contadors per tal que no influeixin
-sed -i '1i"id,title,type,description,release_year,age_certification,runtime,genres,production_countries, ,imdb_id,imdb_score,imdb_votes,tmdb_popularity,tmdb_score,imdb_reliability,tmdb_reliability"' Movies.csv
-sed -i '1i"id,title,type,description,release_year,age_certification,runtime,genres,production_countries,seasons,imdb_id,imdb_score,imdb_votes,tmdb_popularity,tmdb_score,imdb_reliability,tmdb_reliability"' Shows.csv
-sed -i '1i"id,title,type,description,release_year,age_certification,runtime,genres,production_countries,seasons,imdb_id,imdb_score,imdb_votes,tmdb_popularity,tmdb_score,imdb_reliability,tmdb_reliability"' MS.csv
+if [ $c != "S" ];
+then
+    # afegim els títols fora dels contadors per tal que no influeixin
+    sed -i '1i"id,title,type,description,release_year,age_certification,runtime,genres,production_countries, ,imdb_id,imdb_score,imdb_votes,tmdb_popularity,tmdb_score,imdb_reliability,tmdb_reliability"' Movies.csv
+    sed -i '1i"id,title,type,description,release_year,age_certification,runtime,genres,production_countries,seasons,imdb_id,imdb_score,imdb_votes,tmdb_popularity,tmdb_score,imdb_reliability,tmdb_reliability"' Shows.csv
+    sed -i '1i"id,title,type,description,release_year,age_certification,runtime,genres,production_countries,seasons,imdb_id,imdb_score,imdb_votes,tmdb_popularity,tmdb_score,imdb_reliability,tmdb_reliability"' MS.csv
+fi
 
 echo "T'agradaria conservar les dades? (S/n)"
 read b 
