9a20c92f150c17ba313ceca7fea8d1018fdddd81
Joancc04 <joancolillas2004@gmail.com>
2022-10-21 23:40:26 +0200

    apartat 5 acabat i 6 pràcticament acabat

--- ext.sh
+++ ext.sh
@@ -7,7 +7,6 @@ elim () {
     rm Debris.csv
     rm Debrisid.csv
     rm Debrisc.csv
-    rm aux.csv
     rm MS.csv
 }
 
@@ -31,13 +30,6 @@ touch aux.csv
 touch aux2.csv
 touch MS.csv
 
-
-
-# sed -i '/MOVIE/d' ShowData.csv 
-# sed -i '/SHOW/d' MovieData.csv 
-# echo Netejant dades...
-
-# -F "," = IFS
 
 #trasllat i dividit de les dades útils
 echo Classificant i netejant dades...
@@ -76,16 +68,26 @@ echo
 awk -F "," 'BEGIN{maxv=0} { if($13 > maxv) maxv=$13} END{print "El número més gran imdb_votes és:", maxv}' aux.csv #troba el màxim de la columna 13
 awk -F "," 'BEGIN{maxp=0} { if($14 > maxp) maxp=$14} END{print "El número més gran tmdb_popularity és:", maxp}' aux.csv #troba el màxim de la columna 14
 echo 
-
+echo 
 sed -i 's/\"//g' MS.csv #borra totes les dobles comes del script, /g per incloure no només les del principi
+echo "Imdb:"
+awk -F "," 'BEGIN{id="";title="";ctry="";sc=0} { if ($12 > sc && $3=="MOVIE") {sc=$12 ; id=$1 ; ctry=$9 ; title=$2}} END{print "Pel·lícula amb millor puntuació IMDB:", id, title, ctry, sc}' MS.csv
+awk -F "," 'BEGIN{id="";title="";ctry="";sc=0} { if ($12 > sc && $3=="SHOW") {sc=$12 ; id=$1 ; ctry=$9 ; title=$2}} END{print "Serie amb millor puntuació IMDB:", id, title, ctry, sc}' MS.csv
+awk -F "," 'BEGIN{id="";title="";ctry="";ivotes=0} { if ($13 > ivotes && $3=="MOVIE") {ivotes=$13 ; id=$1 ; title=$2 ; ctry=$9}} END{print "Pel·lícula més popular segons IMDB:", id, title, ctry, ivotes}' MS.csv
+awk -F "," 'BEGIN{id="";title="";ctry="";ivotes=0;isc=0} { if ($13 > ivotes && $3=="SHOW") {ivotes=$13 ; id=$1 ; title=$2 ; ctry=$9; isc=$12}} END{print "Serie més popular segons IMDB:",  id, title, ctry, isc}' MS.csv
+awk -F "," 'BEGIN{id="";title="";ctry="";coe=0;isc=0} { if ($16 >= coe && $3=="MOVIE") {coe=$16 ; id=$1 ; title=$2 ; ctry=$9 ; isc=$12}} END{print "Pel·lícula amb el millor coeficient IMDB:", id, title, ctry, coe}' MS.csv
+echo
+echo "Tmdb:"
+awk -F "," 'BEGIN{id="";title="";ctry="";sc=0} { if ($15 > sc && $3=="MOVIE") {sc=$15 ; id=$1 ; ctry=$9 ; title=$2}} END{print "Pel·lícula amb millor puntuació TMDB:", id, title, ctry, sc}' MS.csv
+awk -F "," 'BEGIN{id="";title="";ctry="";sc=0} { if ($15 > sc && $3=="SHOW") {sc=$15 ; id=$1 ; ctry=$9 ; title=$2}} END{print "Serie amb millor puntuació TMDB:", id, title, ctry, sc}' MS.csv
+awk -F "," 'BEGIN{id="";title="";ctry="";tmp=0} { if ($14 > tmp && $3=="MOVIE") {tmp=$14 ; id=$1 ; title=$2 ; ctry=$9}} END{print "Pel·lícula més popular segons TMDB:", id, title, ctry, tmp}' MS.csv
+awk -F "," 'BEGIN{id="";title="";ctry="";tmp=0;isc=0} { if ($14 > tmp && $3=="SHOW") {tmp=$14 ; id=$1 ; title=$2 ; ctry=$9; isc=$12}} END{print "Serie més popular segons TMDB:",  id, title, ctry, tmp}' MS.csv
+awk -F "," 'BEGIN{id="";title="";ctry="";coe=0;isc=0} { if ($17 > coe && $3=="MOVIE") {coe=$17 ; id=$1 ; title=$2 ; ctry=$9 ; isc=$12}} END{print "Pel·lícula amb el millor coeficient TMDB:", id, title, ctry, coe}' MS.csv
+awk -F "," 'BEGIN{id="";title="";ctry="";coe=0} { if ($17 > coe && $3=="SHOW") {coe=$17 ; id=$1 ; title=$2 ; ctry=$9}} END{print "La serie amb el millor coeficient TMDB és:", id, title, ctry, coe}' MS.csv
 
-awk -F "," 'BEGIN{a=0} { if ($12 > a && $3=="MOVIE") a=$12} END{print "La pel·lícula amb la millor puntuació imdb és:", a}' MS.csv
-awk -F "," 'BEGIN{b=0} { if ($12 > b && $3=="SHOW") b=$12} END{print "La serie amb la millor puntuació imdb és:", b}' MS.csv
-awk -F "," 'BEGIN{c=0} { if ($13 > c && $3=="MOVIE") c=$13} END{print "La pel·lícula més popular segons imdb és:", c}' MS.csv
-awk -F "," 'BEGIN{d=0} { if ($13 > d && $3=="SHOW") d=$13} END{print "La serie més popular segons imdb és:", d}' MS.csv
-awk -F "," 'BEGIN{e=0} { if ($16 > e && $3=="MOVIE") e=$16} END{print "La pel·lícula amb el millor coeficient Imdb és:", e}' MS.csv
-awk -F "," 'BEGIN{f=0} { if ($16 > f && $3=="SHOW") f=$16} END{print "La serie amb el millor coeficient IMDB és:", f}' MS.csv
 
+rm aux.csv
+rm aux2.csv
 
 echo "Títols als documents? (S/n)"
 read c
@@ -94,7 +96,6 @@ then
     # afegim els títols fora dels contadors per tal que no influeixin
     sed -i '1iid,title,type,description,release_year,age_certification,runtime,genres,production_countries, ,imdb_id,imdb_score,imdb_votes,tmdb_popularity,tmdb_score,imdb_reliability,tmdb_reliability' Movies.csv
     sed -i '1iid,title,type,description,release_year,age_certification,runtime,genres,production_countries,seasons,imdb_id,imdb_score,imdb_votes,tmdb_popularity,tmdb_score,imdb_reliability,tmdb_reliability' Shows.csv
-    sed -i '1iid,title,type,description,release_year,age_certification,runtime,genres,production_countries,seasons,imdb_id,imdb_score,imdb_votes,tmdb_popularity,tmdb_score,imdb_reliability,tmdb_reliability' aux.csv
     sed -i '1iid,title,type,description,release_year,age_certification,runtime,genres,production_countries,seasons,imdb_id,imdb_score,imdb_votes,tmdb_popularity,tmdb_score,imdb_reliability,tmdb_reliability' MS.csv
 
 fi
@@ -106,6 +107,5 @@ then
     elim
     echo "S'han borrat les dades"
 fi
-rm aux.csv
-rm aux2.csv
+
 
